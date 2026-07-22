a5c2e9c961ba2024db3c1eda500f46bfff2a96da
tianbingcha2025 <B25040520@njupt.edu.cn>
2026-07-22 23:44:54 +0800

    chore:WeatherFunction into weather

--- src/main/java/com/youkeda/exercise/claw/weather/WeatherFunction.java
+++ src/main/java/com/youkeda/exercise/claw/weather/WeatherFunction.java
@@ -1,10 +1,10 @@
-package com.youkeda.exercise.claw.agent.tool;
+package com.youkeda.exercise.claw.weather;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.youkeda.exercise.claw.weather.WeatherResponse;
-import com.youkeda.exercise.claw.weather.WeatherTool;
+import com.youkeda.exercise.claw.agent.tool.LLMFunction;
+import com.youkeda.exercise.claw.agent.tool.LLMFunctionRegistry;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
